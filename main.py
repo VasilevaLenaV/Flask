ef7a5f63a8f1039801f73d6c713315de287d686a
VasilevaLenaV <charoit@icloud.com>
2023-08-17 20:02:26 +0300

    Дз к уроку 2

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, flash, redirect, make_response
 
 app = Flask(__name__)
 
+package_dir = os.path.dirname(
+    os.path.abspath(__file__)
+)
+static = os.path.join(
+    package_dir, "static"
+)
+app.static_folder=static
+app.config['SECRET_KEY'] = 'qwerty!QAZ'
+
 category = [
-    {"name": "Одежда",  "id": 1},
+    {"name": "Одежда", "id": 1},
     {"name": "Обувь", "id": 2},
     {"name": "Аксессуары", "id": 3},
 ]
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user_data = request.cookies.get('user_data')
+    if user_data:
+        cookies = (dict(i.split('=', 1) for i in user_data.split('&')))
+        return render_template('index.html', user=cookies)
+    return render_template('index.html', user=None)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user_data = request.cookies.get('user_data')
+    if user_data:
+        cookies = (dict(i.split('=', 1) for i in user_data.split('&')))
+        return render_template('login.html', user=cookies)
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+
+        if username and email:
+            flash("Successful login", "success")
+            response = make_response(redirect('/'))
+            response.set_cookie('user_data', f'username={username}&email={email}')
+            return response
+        else:
+            flash("Wrong username or email", "danger")
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    response = make_response(redirect('/'))
+    response.delete_cookie('user_data')
+    return response
 
 
 @app.route('/categories')
